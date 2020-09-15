6a0c37269e427c0c17b73a29eccccf7cd169a914
deadfiesta <deadfiesta@gmailcom>
2020-09-15 15:46:00 +0800

    Update JS/CSS

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
+//shorthand
+var $ = function( id ) { 
+    return document.getElementById( id );
+};
 
-$('#cpu').click(function() {
-    window.open('https://www.amd.com/en/products/cpu/amd-ryzen-7-3700x', '_blank')
+//const
+const nzxtCase = document.querySelector(".hero-right");
+const heroText = document.querySelector(".hero-left");
+const descriptionText = document.querySelector(".description");
+
+
+//click events
+$('cpu').addEventListener("click", () => {
+    window.open("https://www.amd.com/en/products/cpu/amd-ryzen-7-3700x", "_blank")
+});
+
+
+//gsap events
+
+gsap.from(nzxtCase, {
+    duration: 2,
+    x: 300,
+    opacity: 0,
+    ease: "power4.out",
+});
+gsap.from(heroText, {
+    duration: 1,
+    x: -100,
+    opacity: 0,
+} )
+
+gsap.from(descriptionText, {
+    duration: 2,
+    opacity: 0,
+    delay: .25,
 })
+
