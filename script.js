e352cb99f8c997becc9bc67605f5cbb72d11f84c
deadfiesta <deadfiesta@gmailcom>
2020-09-15 17:16:43 +0800

    Updated content and CSS

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
-//shorthand
-var $ = function( id ) { 
-    return document.getElementById( id );
-};
+"use strict";
 
 //const
 const nzxtCase = document.querySelector(".hero-right");
 const heroText = document.querySelector(".hero-left");
 const descriptionText = document.querySelector(".description");
+const priceTag = document.querySelector(".pricing");
 
+//cal total cost pricing
+var sum = 0;
+var buy = 0;
+$('.pricing').each(function() {
+    var val = $.trim( $(this).text() );
+
+    if ( val ) {
+        val = parseFloat( val.replace( /^\$/, "" ) );
+
+        sum += !isNaN( val ) ? val : 0;
+    }
+});
+const cost = (Math.round(sum*100)/100).toFixed(2);
+$('.total-pricing').html("S$"+cost);
+//cal total purchased 
+$('.purchased').each(function() {
+    var val = $.trim( $(this).text() );
+
+    if ( val ) {
+        val = parseFloat( val.replace( /^\$/, "" ) );
+
+        buy += !isNaN( val ) ? val : 0;
+    }
+});
+const bought = (Math.round(buy*100)/100).toFixed(2);
+$('.total-purchased').html("S$"+bought);
+
+$('.pricing').prepend("S$");
 
 //click events
-$('cpu').addEventListener("click", () => {
+$('#cpu').click(() => {
     window.open("https://www.amd.com/en/products/cpu/amd-ryzen-7-3700x", "_blank")
 });
-
+$('#aio').click(() => {
+    window.open("https://www.nzxt.com/products/kraken-x53", "_blank")
+});
 
 //gsap events
-
 gsap.from(nzxtCase, {
     duration: 2,
-    x: 300,
+    x: "+=100",
     opacity: 0,
     ease: "power4.out",
 });
 gsap.from(heroText, {
     duration: 1,
-    x: -100,
+    x: "-=100",
     opacity: 0,
 } )
 
